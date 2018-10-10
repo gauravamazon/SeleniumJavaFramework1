a9e7884da020e70173a54a941681488cf2a7db0f
kulkgaur <kulkgaur@HY16-1800223603.ant.amazon.com>
2018-10-10 16:05:56 +0530

    Added githubdemo class

---
+++ SeleniumJavaFramework/src/test/java/demo/GithubDemo.java
@@ -0,0 +1,13 @@
+package demo;
+
+public class GithubDemo {
+	
+	public static void main(String[] args) {
+		githubdemo();
+	}
+
+	public static void githubdemo() {
+		
+		System.out.println("Hello");
+	}
+}
